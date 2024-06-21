abb7753149b432e2fcffbf554f616217a79c4dd2
Roque Buarque <roqueb@spotify.com>
2024-06-21 09:57:42 +0100

    artist row

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -3,12 +3,18 @@ package com.devspacecomposeinit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.devspacecomposeinit.ui.theme.ComposeInitTheme
 
@@ -36,6 +42,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun ArtistCardRow(artist: Artist) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = artist.image), contentDescription = "Artist image")
+        Column {
+            Text(artist.name)
+            Text(artist.lastSeenOnline)
+        }
+    }
+}
+
+data class Artist(
+    val name: String,
+    val lastSeenOnline: String,
+    @DrawableRes val image: Int,
+    @DrawableRes val art: Int
+)
 
 @Preview(showBackground = true)
 @Composable
