3c7f052a2116957f00281ac46f34199707753317
Roque Buarque <roqueb@spotify.com>
2024-06-21 10:31:57 +0100

    artist list

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -5,17 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devspacecomposeinit.ui.theme.ComposeInitTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +47,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val picasso = Artist(
+                        name = "Pablo Picasso",
+                        lastSeenOnline = "3 min",
+                        image = R.drawable.ic_pablo_picasso,
+                        art = R.drawable.ic_mona_lisa
+                    )
+
+                    val leonardo = Artist(
+                        name = "Leonardo da Vinci",
+                        lastSeenOnline = "4 min",
+                        image = R.drawable.ic_leonardo_da_vinci,
+                        art = R.drawable.ic_mona_lisa
+                    )
+
+                    val salvador = Artist(
+                        name = "Salvador Dali",
+                        lastSeenOnline = "5 min",
+                        image = R.drawable.ic_salvador_dali,
+                        art = R.drawable.ic_persistence_of_memory
+                    )
+
+                    val vanGogh = Artist(
+                        name = "Vicent Van Gogh",
+                        lastSeenOnline = "15 min",
+                        image = R.drawable.ic_vincent_van_gogh,
+                        art = R.drawable.ic_starry_night
+                    )
+
+                    val artists = listOf(vanGogh, leonardo, salvador, picasso)
+
+                    LazyColumn {
+                        items(artists) { artist ->
+                            ArtistCardRow(artist = artist) {
+
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -36,20 +91,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun ArtistCardRow(artist: Artist) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(painter = painterResource(id = artist.image), contentDescription = "Artist image")
-        Column {
-            Text(artist.name)
-            Text(artist.lastSeenOnline)
+fun ArtistCardRow(
+    artist: Artist,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillWidth,
+                painter = painterResource(id = artist.image), contentDescription = "Artist image"
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Column {
+                Text(
+                    text = artist.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Text(
+                    text = artist.lastSeenOnline, color = Color.Gray
+                )
+            }
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .clip(shape = RoundedCornerShape(2.dp)),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = artist.art), contentDescription = "Artist art"
+            )
         }
     }
 }
@@ -63,8 +149,16 @@ data class Artist(
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtistCardRowPreview() {
     ComposeInitTheme {
-        Greeting("Android")
+        val leonardo = Artist(
+            name = "Leonardo da Vinci",
+            lastSeenOnline = "3 min",
+            image = R.drawable.ic_leonardo_da_vinci,
+            art = R.drawable.ic_mona_lisa
+        )
+        ArtistCardRow(artist = leonardo) {
+
+        }
     }
 }
